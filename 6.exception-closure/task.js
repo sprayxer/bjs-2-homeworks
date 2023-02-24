ae85f894c85bdc47fb7ac7773db6ef9f39be704f
sprayxer <89803128394@mail.ru>
2023-02-24 11:42:24 +0300

    add 6.exception-closure

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,54 @@
-﻿
+﻿function parseCount(quantityОfGoods){
+    if(isNaN(Number.parseFloat(quantityОfGoods))){
+        const error = new Error ('Невалидное значение')
+        throw error;
+    }
+    return Number.parseFloat(quantityОfGoods);
+}
+
+function validateCount(quantityОfGoods){
+    try{
+        return parseCount(quantityОfGoods);
+        
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a,b,c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if(this.a + this.b < this.c || this.a + this.c < this.b || this.b + this.c < this.a){
+            const error = new Error ('Треугольник с такими сторонами не существует');
+            throw error;
+        }
+    }
+
+    get perimeter(){
+        return this.a + this.b + this.c;
+    }
+    
+    get area(){
+        const p = (this.a + this.b + this.c) / 2;
+        return Number((Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3));
+    }
+}
+    function getTriangle(a,b,c) {
+        try {
+            let triangle = new Triangle(a,b,c);                
+            return triangle;
+        } catch (error) {
+            triangle = {
+                get area() {
+                    return 'Ошибка! Треугольник не существует';
+                },
+                get perimeter() {
+                    return 'Ошибка! Треугольник не существует';
+                }
+            }
+            return triangle;
+        }
+    }
+
